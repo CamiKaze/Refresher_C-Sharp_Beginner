44abd9e72a31dcc2641113d96548f7918b9fee3a
CamiKaze <cameronsmith63@gmail.com>
2020-03-22 11:01:33 +0200

    Added FileAndFileInfo Project. Showing the difference between static and instance methods

---
+++ Files/FileAndFileInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace FileAndFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // File provides static methods.
+            var path = @"C:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            // FileInfo provides instance methods
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+        }
+    }
+}
