7c95aaa3405809ab373b5d3173997cf47b438179
CamiKaze <cameronsmith63@gmail.com>
2020-03-22 11:25:43 +0200

    Added DirectoryAndDirectoryInfo Project. Comparing Static and Instance methods

---
+++ Directories/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* It is more convenient to use static methods, but everytime you use them,
+            the Operating System does some security checking. This can have an effect
+            the performance of your application. To avoid the performance issue, it is
+            more effecient to create a DirectoryInfo object and call the instance methods
+            on that object */
+            
+            Directory.CreateDirectory(@"c:\temp\folder1\asd");
+            Directory.Delete(@"c:\temp\", true);
+
+            var files = Directory.GetFiles(@"c:\Intel", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                System.Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\Intel", "*.*",
+             SearchOption.AllDirectories);
+
+            foreach (var directory in directories)
+                System.Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
