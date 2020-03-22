a6f4053ec05b557993013c65b0bdbf961873763b
CamiKaze <cameronsmith63@gmail.com>
2020-03-22 11:35:33 +0200

    Added Path project

---
+++ Path/PathMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace PathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Windows\apppatch\frxmain.sdb";
+            System.Console.WriteLine("Extension: " + Path.GetExtension(path));
+            System.Console.WriteLine("File Name: " + Path.GetFileName(path));
+            System.Console.WriteLine("File Name without extension: " + Path.GetFileNameWithoutExtension(path));
+            System.Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
